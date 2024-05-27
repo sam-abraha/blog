f1771717768ff91f1842090c0d1a3ea39bd78b26
sam <samuelabraha@gmail.com>
2024-05-27 20:00:56 +0200

    Initial commit; Add header and post skeleton

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from './components/Header.jsx'
+import Post from './components/Post.jsx'
+
+function App() {
+
+  return (
+    <main className='p-10 max-w-screen-md ml-auto mr-auto'>
+      <Header/>
+      <Post/>
+    </main>
+  )
+}
+
+export default App
---
+++ client/src/components/Post.jsx
@@ -0,0 +1,22 @@
+export default function Post() {
+    return (
+        <div className="mt-12 grid grid-cols-2 gap-5 text-left ">
+        <div className="col-span-1">
+          <img src="https://techcrunch.com/wp-content/uploads/2024/02/IMG_0247.jpeg?resize=1536,1152"
+          className="w-full max-h-full"></img>
+        </div>
+        <div className="col-span-1">
+          <h2 className="font-bold text-[1.4rem] m-0">Google adds live threat detection and screen-sharing protection to Android</h2>
+          <p className="text-gray-600 font-bold text-xs mt-1 mb-1 flex gap-1">
+            <a>Ivan Metha</a>
+            <time>04-15-2024 10am</time>
+          </p>
+          <p className="flex text-gray-900 text-sm">Google said on Wednesday that it is adding new security and privacy protections to Android,
+            including on-device live threat detection to catch malicious apps, 
+            new safeguards and security against cell site simulators.
+            </p>   
+        </div>
+        <div style={{ borderTop: "2px solid #d1d5db ", marginLeft: 10, marginRight: 10 }}></div>
+      </div>
+    )
+}
