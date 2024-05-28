d8f232933a8d49b4cc4215829b4a1741599513b3
sam <samuelabraha@gmail.com>
2024-05-28 22:56:50 +0200

    Add signin endpoint

--- client/src/pages/SigninPage.jsx
+++ client/src/pages/SigninPage.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react"
+import axios from "axios"
+
 export default function SigninPage() {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function signin(e) {
+        e.preventDefault()
+        const response = await fetch("http://localhost:3000/signin", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+          });
+        if(response.ok===200) {
+            alert("Registration successful")
+        }else {
+            alert("Registration failed")
+        }
+    }
+
     return (
-        <form className="block mt-24 max-w-md mx-auto text-center ">
+        <form className="block mt-24 max-w-md mx-auto text-center" onSubmit={signin}>
             <h1 className="text-center text-lime-600 font-bold text-4xl mb-4">Sign in</h1>
             <input 
-            type="email" 
-            placeholder="Email" 
-            className="block w-full p-4 border rounded-lg"/>
+            type="name" 
+            placeholder="Name" 
+            className="block w-full p-4 border rounded-lg"
+            value={username}
+            onChange={e => setUsername(e.target.value)}/>
             <input 
             type="password" 
             placeholder="Password" 
-            className="block w-full p-4 border rounded-lg"/>
+            className="block w-full p-4 border rounded-lg"
+            value={password}
+            onChange={e => setPassword(e.target.value)}/>
             <button className="block w-full p-4 border rounded-lg text-white bg-lime-600 hover:bg-lime-700 mt-4">
                 Sign in
             </button>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,58 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+const PORT = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Test');
 });
 
+
+async function deleteAllUsers() {
+  try {
+    const result = await prisma.user.deleteMany({});
+    console.log(`Deleted ${result.count} users`);
+  } catch (error) {
+    console.error('Error deleting users:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function createUser(username: string, password: string) {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: username,
+        password: password,
+      },
+    });
+    console.log('User created:', user);
+    return user;
+  } catch (error) {
+    console.error('Error creating user:', error);
+  }
+}
+
+
+app.post('/signin', async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+    const user = await createUser(username, password);
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 // Starts the server
 app.listen(PORT, () => {
